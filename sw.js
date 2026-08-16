62e3aa7d8863e8803ad5d645b4ef26a7c1d39e8d
DurgaRabidas <laxmirabidas9@gmail.com>
2026-08-16 19:52:18 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,34 @@
+const CACHE_NAME = 'photo-editor-v2'; // version badha diya
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open('photo-editor-v1'));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting(); // turant activate ho jaye
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)) // purana cache delete
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
 });
